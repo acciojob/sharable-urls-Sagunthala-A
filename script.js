799569b649667465f3430eb0e424766433bb38d3
Sagunthala A <117196512+Sagunthala-A@users.noreply.github.com>
2024-03-20 07:34:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // your code here
+let btn = document.getElementById("button");
+let h3 = document.getElementById("url");
+let name = document.getElementById("name");
+let year = document.getElementById("year");
+
+
+btn.addEventListener("click",()=>{
+	h3.innerText = `hi`
+	if(name.target.value && year.target.value) {
+		h3.innerText = `hi`
+	}
+})
