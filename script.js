088e3c64f1923933d44e85cbce5f888768fb7033
Sagunthala A <117196512+Sagunthala-A@users.noreply.github.com>
2024-03-22 17:44:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,9 +5,14 @@ let name = document.getElementById("name");
 let year = document.getElementById("year");
 
 
-btn.addEventListener("click",()=>{
-	h3.innerText = `hi`
-	if(name.target.value && year.target.value) {
-		h3.innerText = `hi`
+btn.addEventListener("click",(e)=>{
+	e.preventDefault();
+	let baseURL = "https://localhost:8080/"
+	if(name.value && year.value){
+		h3.textContent = `${baseURL}?name=${name.value}&year=${year.value}`
+	}else if(name.value){
+		h3.textContent = `${baseURL}?name=${name.value}`
+	}else if(year.value){
+		h3.textContent = `${baseURL}?name=${year.value}`
 	}
 })
